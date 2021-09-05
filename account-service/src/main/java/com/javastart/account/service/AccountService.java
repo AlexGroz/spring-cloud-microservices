1c8f9b8dbbb47385f49f1045e1b22203f9bed119
Alexey <groznovaa@gmail.comls>
2021-09-05 12:50:08 +0500

    added value AccountRepository, constructor, and method getAccountById

--- account-service/src/main/java/com/javastart/account/service/AccountService.java
+++ account-service/src/main/java/com/javastart/account/service/AccountService.java
@@ -1,7 +1,24 @@
 package com.javastart.account.service;
 
+import com.javastart.account.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public AccountRepository getAccountById(Long accountId){
+        return accountRepository.findById(accountId).orElseThrow()
+    }
 }
+
+
+
