fc10a009b1b76682a88ed27bd05cd496eb3ee409
Alexey <groznovaa@gmail.comls>
2021-09-05 10:45:10 +0500

    added @GetMapping AccountResponseDTO in AccountController

--- account-service/src/main/java/com/javastart/account/controller/AccountController.java
+++ account-service/src/main/java/com/javastart/account/controller/AccountController.java
@@ -1,7 +1,15 @@
 package com.javastart.account.controller;
 
+import com.javastart.account.controller.dto.AccountResponseDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+
+    @GetMapping("/{accountId}")
+    public AccountResponseDTO getAccount(@PathVariable Long accountId){
+
+    }
 }
