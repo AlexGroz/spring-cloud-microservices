f7c8046a75de07e1e72e76abab8d10a9aa866736
Alexey <groznovaa@gmail.comls>
2021-09-05 10:40:48 +0500

    added values to AccountRequestDTO

--- account-service/src/main/java/com/javastart/account/controller/dto/AccountRequestDTO.java
+++ account-service/src/main/java/com/javastart/account/controller/dto/AccountRequestDTO.java
@@ -1,4 +1,19 @@
 package com.javastart.account.controller.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@NoArgsConstructor
 public class AccountRequestDTO {
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private OffsetDateTime creationDate;
 }
--- account-service/src/main/java/com/javastart/account/controller/dto/AccountResponseDTO.java
+++ account-service/src/main/java/com/javastart/account/controller/dto/AccountResponseDTO.java
@@ -1,12 +1,19 @@
 package com.javastart.account.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.OffsetDateTime;
 
+@Getter
+@AllArgsConstructor
 public class AccountResponseDTO {
 
     private Long accountId;
 
     private String name;
+
+    private String email;
 
     private  String phone;
 
