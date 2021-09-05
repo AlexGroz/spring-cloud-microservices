eb40e01fe4c8e8f7452ee671c1e57fc41bb0b005
Alexey <groznovaa@gmail.comls>
2021-09-05 17:29:06 +0500

    Created @GetMapping getBill method

--- bill-service/src/main/java/com/javastart/bill/controller/BillController.java
+++ bill-service/src/main/java/com/javastart/bill/controller/BillController.java
@@ -1,7 +1,24 @@
 package com.javastart.bill.controller;
 
+import com.javastart.bill.controller.dto.BillResponseDTO;
+import com.javastart.bill.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BillController {
+
+    private final BillService billService;
+
+    @Autowired
+    public BillController(BillService billService) {
+        this.billService = billService;
+    }
+
+    @GetMapping("/{billId}")
+    public BillResponseDTO getBill(@PathVariable Long billId){
+        return new BillResponseDTO(billService.getBillById(billId));
+    }
 }
