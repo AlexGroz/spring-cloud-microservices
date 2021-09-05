5bae7814fed6ab47ce8a00c4f24e6b1334a44171
Alexey <groznovaa@gmail.comls>
2021-09-05 17:24:12 +0500

    Created BillRequestDTO class with values and constructors

--- bill-service/src/main/java/com/javastart/bill/controller/dto/BillRequestDTO.java
+++ bill-service/src/main/java/com/javastart/bill/controller/dto/BillRequestDTO.java
@@ -1,4 +1,22 @@
 package com.javastart.bill.controller.dto;
 
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
 public class BillRequestDTO {
+
+    private Long accountId;
+
+    private BigDecimal amount;
+
+    private Boolean isDefault;
+
+    private OffsetDateTime creationDate;
+
+    private Boolean overdraftEnabled;
+
+
 }
