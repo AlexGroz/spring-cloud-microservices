99ff8e2b0d155718a6c36d4dc428e1fdbff1f619
Alexey <groznovaa@gmail.comls>
2021-09-05 13:04:08 +0500

    added createAccount method but have not finished one

--- account-service/src/main/java/com/javastart/account/service/AccountService.java
+++ account-service/src/main/java/com/javastart/account/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.javastart.account.service;
 
+import com.javastart.account.entity.Account;
+import com.javastart.account.exception.AccountNotFoundException;
 import com.javastart.account.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -15,8 +19,13 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
-    public AccountRepository getAccountById(Long accountId){
-        return accountRepository.findById(accountId).orElseThrow()
+    public Account getAccountById(Long accountId){
+        return accountRepository.findById(accountId)
+                .orElseThrow(() -> new AccountNotFoundException("Unable to find account with id: " + accountId));
+    }
+
+    public Long createAccount(String name, String email, String phone, List<Long> bills){
+        Account account = new Account(name, email, phone, bills);
     }
 }
 
