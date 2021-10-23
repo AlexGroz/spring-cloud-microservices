a2cce7d0365a05a9563faa40f5c66c7823884dff
AlexGroz <groznovaa@gmail.com>
2021-10-23 18:17:51 +0500

    Many changes

---
+++ deposit-service/src/main/java/com/javastart/deposit/rest/AccountServiceClient.java
@@ -0,0 +1,7 @@
+package com.javastart.deposit.rest;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient
+public interface AccountServiceClient {
+}
---
+++ deposit-service/src/main/java/com/javastart/deposit/service/DepositService.java
@@ -0,0 +1,10 @@
+package com.javastart.deposit.service;
+
+import com.javastart.deposit.repository.DepositRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepositService {
+
+    private final DepositRepository repository;
+}
